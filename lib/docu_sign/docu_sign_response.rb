7c835ca83633130f46538c4cbb57e0727d2aa939
Donald Piret <developers+donaldpiret@roomorama.com>
2011-11-17 16:40:04 +0800

    Remove a puts

--- lib/docu_sign/docu_sign_response.rb
+++ lib/docu_sign/docu_sign_response.rb
@@ -4,7 +4,6 @@ module DocuSign
 
     def self.new(*args)
       response = args.first
-      puts response.to_hash.inspect
       case response.to_hash.keys.first.to_sym
         when :create_and_send_envelope_response
           return EnvelopeStatus.new(response.to_hash[:create_and_send_envelope_response][:create_and_send_envelope_result])
