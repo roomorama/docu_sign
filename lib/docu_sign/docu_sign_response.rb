00231473cf450a8cb2f816b214b63541c9d11ae1
Donald Piret <developers+donaldpiret@roomorama.com>
2011-11-18 18:24:36 +0800

    Add support for request_status_ex

--- lib/docu_sign/docu_sign_response.rb
+++ lib/docu_sign/docu_sign_response.rb
@@ -9,6 +9,8 @@ module DocuSign
           return EnvelopeStatus.new(response.to_hash[:create_and_send_envelope_response][:create_and_send_envelope_result])
         when :request_status_response
           return EnvelopeStatus.new(response.to_hash[:request_status_response][:request_status_result])
+        when :request_status_ex_response
+          return EvenlopeStatus.new(response.to_hash[:request_status_ex_response][:request_status_ex_result])
         when :request_envelope_response
           return Envelope.new(response.to_hash[:request_envelope_response][:request_envelope_result])
         when :correct_and_resend_envelope_response
