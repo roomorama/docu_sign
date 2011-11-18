f3c4beec65a19684f267e2fe82a4e9b7530f2545
Donald Piret <developers+donaldpiret@roomorama.com>
2011-11-18 18:16:03 +0800

    Add support for request_status_response

--- lib/docu_sign/docu_sign_response.rb
+++ lib/docu_sign/docu_sign_response.rb
@@ -7,8 +7,12 @@ module DocuSign
       case response.to_hash.keys.first.to_sym
         when :create_and_send_envelope_response
           return EnvelopeStatus.new(response.to_hash[:create_and_send_envelope_response][:create_and_send_envelope_result])
+        when :request_status_response
+          return EnvelopeStatus.new(response.to_hash[:request_status_response][:request_status_result])
         when :request_envelope_response
           return Envelope.new(response.to_hash[:request_envelope_response][:request_envelope_result])
+        when :correct_and_resend_envelope_response
+          return nil
         else
           puts response.to_hash.inspect
           super
