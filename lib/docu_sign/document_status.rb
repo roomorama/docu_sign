8dfea61a21c47919ac2e6f386a671c18509ffdaf
Donald Piret <developers+donaldpiret@roomorama.com>
2011-11-18 18:31:29 +0800

    Missing DocumentStatus class

---
+++ lib/docu_sign/document_status.rb
@@ -0,0 +1,16 @@
+module DocuSign
+
+  class DocumentStatus < DocuSignModel
+    ATTRIBUTES = [:id, :name, :template_name, :sequence]
+  end
+  ATTRIBUTES.each do |attr|
+    self.send(:attr_accessor, attr)
+  end
+
+  def initialize(attributes = {})
+    ATTRIBUTES.each do |attr|
+      self.send("#{attr}=", attributes[attr])
+    end
+  end
+
+end
